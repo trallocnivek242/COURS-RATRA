9cdff1a569213d1b1b1029ed64230b83f35b69ad
trallocnivek242 <collart.1510.student@ifosupwawre.be>
2019-06-25 16:02:41 +0200

    Update concatenation.php

--- EXEMPLES/PHP/concatenation.php
+++ EXEMPLES/PHP/concatenation.php
@@ -2,8 +2,10 @@
 	// APPRENDRE A CONCATENER DANS UNE VARIABLE PHP
 	// PS: CONCATENER VEUT DIRE ASSEMBLAGE
 	// --------------------------------------------
-	
+	echo '<h1>La concatenatin: </h1>'
+
 	// EX 1: LA CONCATENATION DU BOURRIN
+	echo '<h2>La methode bourinne !!! </h2>';
 	// ---------------------------------
 	$montext = 'text';
 	$monnombre = 5;
@@ -15,6 +17,7 @@
 	echo (isset($_GET['var']) ? $_GET['var'] : '');
 
 	// EX 2: LA CONCATENATION DU MALIN A
+	echo '<h2>La methode malin A !!! </h2>';
 	// tres bien pour les concatenation courte et sans ternaire (condition) incrustrer
 	// -------------------------------------------------------------------------------
 	$mavar = 'text';
@@ -33,6 +36,7 @@
 	echo "texte $monnombre $mavar $macondition";
 
 	// EX 3: LA CONCATENATION DU MALIN B
+	echo '<h2>La methode malin B !!! </h2>';
 	// perso je prefere celle-ci pour question de lisibilite sin on a des concatenations ultra-longues et si on veux ajouter des ternaires (condition)
 	// -----------------------------------------------------------------------------------------------------------------------------------------------
 	$mavar = 'text';
